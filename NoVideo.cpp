6b3a11a8c492c2492e7c293f3e58ee3bfe79b952
Aurumaker72 <armkdev72@gmail.com>
2021-03-27 13:17:27 +0100

    No video plugin

--- NoVideo.cpp
+++ NoVideo.cpp
@@ -1,20 +1,20 @@
 #include "pch.h"
 #include <string.h>
 
-#include "NoInput.h"
+#include "NoVideo.h"
 #include <minwindef.h>
 #include <Windows.h>
 
 
 EXPORT void CALL GetDllInfo(PLUGIN_INFO* PluginInfo)
 {
-	PluginInfo->Version = 0x0101; // 0x0100
-	PluginInfo->Type = PLUGIN_TYPE_CONTROLLER;
+	PluginInfo->Version = 0x100; // 0x0100
+	PluginInfo->Type = PLUGIN_TYPE_GFX;
 	wsprintfA(PluginInfo->Name, PLUGIN_NAME); // wsprintf will not work because this confuses the preprocessor
 }
 
 EXPORT void CALL DllAbout(HWND hParent)
 {
-	MessageBox(hParent, L"No input\nby Aurumaker72", L"", MB_ICONINFORMATION | MB_OK);
+	MessageBox(hParent, L"No video\nby Aurumaker72", L"", MB_ICONINFORMATION | MB_OK);
 }
 
